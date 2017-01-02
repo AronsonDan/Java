869d1e16e478dc3d7f1543f87b01fd4ad18c6d26
AronsonDan <dan.ar27@gmail.com>
2017-01-02 15:51:56 +0200

    20170102

--- Java/src/exercise20170102/Main.java
+++ Java/src/exercise20170102/Main.java
@@ -35,6 +35,7 @@ public class Main {
 
 	
 	school.printAll();
+	school.printAllWithIterator();
 
     }
 
--- Java/src/exercise20170102/School.java
+++ Java/src/exercise20170102/School.java
@@ -5,16 +5,17 @@ package exercise20170102;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 
 /**
  * @author Dan Aronson
  *
  */
 public class School {
-    
+
     // Member declaration
     private ArrayList<Student> studentList = new ArrayList<Student>();
-    
+
     // Class methods
     public void addStudent(Student student) {
 	if (student == null) {
@@ -24,16 +25,25 @@ public class School {
 	}
     }
 
-    
-    public void sortStudentList(){
-	Collections.sort(this.studentList); 
+    public void sortStudentList() {
+	Collections.sort(this.studentList);
     }
-    
-    public void printAll(){
-	// Prints all of the members of the student array list 
+
+    public void printAll() {
+	// Prints all of the members of the student array list
 	this.sortStudentList();
-	for (int i = 0; i < studentList.size(); i++) {
-	    System.out.println(studentList.get(i));
+	// for each loop over a collection
+	for (Student student : studentList) {
+	    System.out.println(student);
+	}
+    }
+    public void printAllWithIterator() {
+	System.out.println("");
+	System.out.println("Printing using an iterator");
+	Iterator<Student> iterator = studentList.iterator();
+	while (iterator.hasNext()) {
+	    Student student = (Student) iterator.next();
+	    System.out.println(student);
 	}
     }
 }
